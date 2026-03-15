ef4a68055aa80d03eff3a36ca91b9b7a7792eafb
k-kukes <158488017+k-kukes@users.noreply.github.com>
2026-03-15 15:12:50 -0400

    started end screen

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour
+{
+    public Text finalScoreText;
+    public Text finalTimeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameManager.Instance != null)
+        {
+            finalScoreText.text = "Final Score: " + GameManager.Instance.score;
+            finalTimeText.text = "Total Time: " + Mathf.FloorToInt(GameManager.Instance.timer);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RestartGame()
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.score = 0;
+            GameManager.Instance.timer = 0f;
+            GameManager.Instance.health = 0;
+        }
+
+        SceneManager.LoadScene(1);
+    }
+}
